d2733b51811de01c6e8221812babf5462b0df9ec
43451626888 <43451626888@SISTEMAS-0001>
2024-09-26 11:11:16 -0300

    Adicionar arquivos de projeto.

---
+++ CursoPOONotas/Nota.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoPOONotas
+{
+    class Nota
+    {
+        public string Descricao { get; set; }
+
+        public void Exibir()
+        {
+            Console.WriteLine(Descricao);
+            Console.WriteLine("------------------------");
+        }
+    }
+}
---
+++ CursoPOONotas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CursoPOONotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gerenciador = new Gerenciador();
+
+            while (true)
+            {
+                Console.WriteLine("1 - Adicionar Nota");
+                Console.WriteLine("2 - Exibir todas as Notas");
+                Console.WriteLine("3 - Sair");
+
+                var opcao = Console.ReadLine()!;
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Digite uma descrição");
+                        var titulo = Console.ReadLine();
+
+                        gerenciador.Adicionar(titulo);
+                        break;
+                    case "2":
+                        gerenciador.ExibirTodas();
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.WriteLine("Opção invalida");
+                        break;
+                }
+            }
+        }
+    }
+}
